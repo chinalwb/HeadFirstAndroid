2c2b2851e8704bdead809fba148072f69e288ecd
Rain Liu <liuwenbin168@126.com>
2018-12-18 11:12:36 +0800

    bound service location listener and location manager and notification channel and notification manager and notification compat builder and intent and pending intent and set content intent and set title set small icon set content set priority set vibrate

--- c19_bound_service/src/main/java/com/chinalwb/c19_bound_service/MainActivity.java
+++ c19_bound_service/src/main/java/com/chinalwb/c19_bound_service/MainActivity.java
@@ -1,18 +1,36 @@
 package com.chinalwb.c19_bound_service;
 
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String PERMISSION_STRING = Manifest.permission.ACCESS_FINE_LOCATION;
 
     private OdometerService odometerService;
     private boolean bound = false;
@@ -40,8 +58,86 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING)
+                == PackageManager.PERMISSION_GRANTED) {
+            // User has already granted the permission
+            bindOdometerService();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, PERMISSION_STRING)) {
+                Toast.makeText(this, "We need location permission for odometer", Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(this, new String[]{PERMISSION_STRING}, 1001);
+        }
+    }
+
+    private void bindOdometerService() {
         Intent intent = new Intent(this, OdometerService.class);
         bindService(intent, serviceConnection, Service.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1001:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    bindOdometerService();
+                } else {
+                    makeNotification();
+                }
+                break;
+        }
+    }
+
+    private void makeNotification() {
+        final NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    "1",
+                    "Odometer",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            notificationManager.createNotificationChannel(channel);
+            builder = new NotificationCompat.Builder(this, channel.getId());
+        }
+
+
+
+        final NotificationCompat.Builder finalBuilder = builder;
+        finalBuilder.setSmallIcon(android.R.drawable.ic_menu_compass)
+                .setContentText("Odometer")
+                .setContentText("Location permission required")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[]{0, 1000})
+                .setAutoCancel(true)
+//                .setLights(Col, 1, 1)
+                .setLights(0xff00ff00, 300, 100)
+        ;
+
+        final Notification notification = finalBuilder.build();
+        notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+
+        notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+        notification.ledARGB = 0xff00ff00;
+        notification.ledOnMS = 300;
+        notification.ledOffMS = 1000;
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent actionPendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        finalBuilder.setContentIntent(actionPendingIntent);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(1000);
+                notificationManager.notify(1001, notification);
+            }
+        }, 20000);
+
     }
 
     private void displayDistance() {
@@ -54,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
                 if (bound && odometerService != null) {
                     distance = odometerService.getDistance();
                 }
-                String distanceStr = String.format(Locale.getDefault(), "%1$,.2f miles", distance);
+                String distanceStr = String.format(Locale.getDefault(), "%1$,.3f KM", distance);
                 distanceView.setText(distanceStr);
                 handler.postDelayed(this, 1000);
             }
--- c19_bound_service/src/main/java/com/chinalwb/c19_bound_service/OdometerService.java
+++ c19_bound_service/src/main/java/com/chinalwb/c19_bound_service/OdometerService.java
@@ -1,23 +1,75 @@
 package com.chinalwb.c19_bound_service;
 
+import android.Manifest;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import java.util.Random;
 
 public class OdometerService extends Service {
 
+    private final static String PERMISSION_STRING = Manifest.permission.ACCESS_FINE_LOCATION;
+
     private final OdometerBinder odometerBinder = new OdometerBinder();
     private final Random random = new Random();
     private float currentDistance;
+
+    private Location lastLocation;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Log.e("XX", "Bound service created!");
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                float distance = location.distanceTo(lastLocation);
+                currentDistance += distance;
+                Log.e("XX", OdometerService.this + " >> current distance in location listener == " + currentDistance);
+                lastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING) == PackageManager.PERMISSION_GRANTED) {
+            Log.e("XX", "request update location");
+            String provider = locationManager.getBestProvider(new Criteria(), true);
+            locationManager.requestLocationUpdates(provider, 1000, 1, locationListener);
+        }
     }
 
     @Nullable
@@ -30,16 +82,24 @@ public class OdometerService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.e("XX", "Bound service destroyed!");
+        if (locationManager != null && locationListener != null) {
+            if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.removeUpdates(locationListener);
+            }
+            locationManager = null;
+            locationListener = null;
+        }
     }
 
     public float getDistance() {
-        currentDistance += random.nextFloat();
+        // currentDistance += random.nextFloat();
+        Log.e("XX", OdometerService.this + ">> get distance == " + this.currentDistance);
         return currentDistance;
     }
 
     public class OdometerBinder extends Binder {
         public OdometerService getOdometer() {
-            return new OdometerService();
+            return OdometerService.this;
         }
     }
 }
